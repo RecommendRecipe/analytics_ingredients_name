77c661ee31118078fd5897d7e487265a886627f3
AdmiralHonda <cetk21025@g.nihon-u.ac.jp>
2021-07-20 10:21:41 +0900

    [fix] linuxとのバージョンエラーを修正

--- work/analytics/test_word2vec.py
+++ work/analytics/test_word2vec.py
@@ -1,7 +1,6 @@
 from gensim.models import word2vec
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus('../data/wiki_wakati.txt')
-
+sentences = word2vec.Text8Corpus('../data/recipe_memo_wakati.txt')
 model = word2vec.Word2Vec(sentences, min_count=20, window=15)
-model.wv.save_word2vec_format("./wiki.vec.pt", binary=True)
+model.wv.save_word2vec_format("./recipe_step.vec.pt", binary=True)
