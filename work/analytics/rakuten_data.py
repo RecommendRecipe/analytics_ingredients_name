1ec5ada3a0ad0ff3d95b0734e85721a96e1ea098
AdmiralHonda <cetk21025@g.nihon-u.ac.jp>
2021-08-06 14:28:20 +0900

    [fix]エラー処理の追加

--- work/analytics/rakuten_data.py
+++ work/analytics/rakuten_data.py
@@ -20,32 +20,34 @@ for menu in tq(recipe_url_list,total=len(recipe_url_list)):
   recipe = {}
   url = base_url + menu
   data = BeautifulSoup(requests.get(url).content,"html.parser")
-  
-  recipe["title"] = get_para(data.find("h1",class_="page_title__text"))
-  # 所要時間
-  recipe["time"] = get_para(data.find("li",class_="recipe_info__time"))
-  # 予算
-  recipe["cost"] = get_para(data.find("li",class_="recipe_info__cost"))
-  recipe["comment"] = get_para(data.find("div",class_="recipe_info_user__comment"))
-  recipe["serving_for"] = get_para(data.find("h2",class_="contents_title_mb"))
-  recipe["ingredients"] = [{"name":ingre.find("span",class_="recipe_material__item_name").text,"amount":ingre.find("span",class_="recipe_material__item_serving").text} for ingre in data.find_all("li",class_="recipe_material__item")]
-  recipe["step"] = [ step.text for step in data.find_all("span",class_="recipe_howto__text")]
-  recipe["sub_comment"] = get_para(data.find("p",class_="recipe_note__trigger_text"))
-  # 料理のコツ
-  recipe["hint"] = get_para(data.find("p",class_="recipe_note__tips_text"))
-  recipe["id"] = data.find("li",class_="recipe_note__id").text[6:]
-  # カテゴリと料理タイプ
-  tmp = data.find_all("dd",class_="relation_info__item")
-  if len(tmp) >= 1:
-    recipe["category"] = tmp[0].text
+  if data != None:
+    pass
   else:
-    recipe["category"] = ""
-  if len(tmp) >= 2:
-    recipe["type"] = tmp[1].text
-  else:
-    recipe["type"] = ""
+    recipe["title"] = get_para(data.find("h1",class_="page_title__text"))
+    # 所要時間
+    recipe["time"] = get_para(data.find("li",class_="recipe_info__time"))
+    # 予算
+    recipe["cost"] = get_para(data.find("li",class_="recipe_info__cost"))
+    recipe["comment"] = get_para(data.find("div",class_="recipe_info_user__comment"))
+    recipe["serving_for"] = get_para(data.find("h2",class_="contents_title_mb"))
+    recipe["ingredients"] = [{"name":ingre.find("span",class_="recipe_material__item_name").text,"amount":ingre.find("span",class_="recipe_material__item_serving").text} for ingre in data.find_all("li",class_="recipe_material__item")]
+    recipe["step"] = [ step.text for step in data.find_all("span",class_="recipe_howto__text")]
+    recipe["sub_comment"] = get_para(data.find("p",class_="recipe_note__trigger_text"))
+    # 料理のコツ
+    recipe["hint"] = get_para(data.find("p",class_="recipe_note__tips_text"))
+    recipe["id"] = data.find("li",class_="recipe_note__id").text[6:]
+    # カテゴリと料理タイプ
+    tmp = data.find_all("dd",class_="relation_info__item")
+    if len(tmp) >= 1:
+      recipe["category"] = tmp[0].text
+    else:
+      recipe["category"] = ""
+    if len(tmp) >= 2:
+      recipe["type"] = tmp[1].text
+    else:
+      recipe["type"] = ""
 
-  recipe_list.append(recipe)
+    recipe_list.append(recipe)
 
 output_data = json.dumps(recipe_list,ensure_ascii=False,indent=2)
 output = open("../data/sep_scry_data/output_rakuten_1.json",'w',encoding='utf-8')
