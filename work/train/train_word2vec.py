ab7382e0371a54d7bad72daf7387e88c7dd2aa49
AdmiralHonda <cetk21025@g.nihon-u.ac.jp>
2021-11-09 17:34:40 +0900

    [update]学習データの前処理を追加

--- work/train/train_word2vec.py
+++ work/train/train_word2vec.py
@@ -1,6 +1,6 @@
 from gensim.models import word2vec
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus('../data/train_data/cookpad_step_corpas.txt')
+sentences = word2vec.Text8Corpus('../data/train_data/recipe_train.txt')
 model = word2vec.Word2Vec(sentences, epochs=100,vector_size=500,sg=1,min_count=5, window=4, workers=8,hs=0)
-model.wv.save_word2vec_format("../data/trained_data/cookpad_m1_v500_min5_w4_ep100.vec.pt", binary=True)
+model.wv.save_word2vec_format("../data/trained_data/cookpad_m1_v500_min5_w4.vec.pt", binary=True)
