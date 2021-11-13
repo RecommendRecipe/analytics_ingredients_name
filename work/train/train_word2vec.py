6a4e3948de034e44079ed3ded0a31941f71105d8
AdmiralHonda <zipangu666@gmail.com>
2021-11-13 21:31:39 +0900

    [update]圧縮次元数を1000まで変化しながら出力できるように調整

--- work/train/train_word2vec.py
+++ work/train/train_word2vec.py
@@ -1,6 +1,11 @@
 from gensim.models import word2vec
 import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus('../data/train_data/cookpad_step_corpas.txt')
-model = word2vec.Word2Vec(sentences, epochs=100,vector_size=500,sg=1,min_count=5, window=4, workers=8,hs=0)
-model.wv.save_word2vec_format("../data/trained_data/cookpad_m1_v500_min5_w4_ep100.vec.pt", binary=True)
+
+sentences = word2vec.Text8Corpus('../data/train_data/rakuten_scray_train.txt')
+
+for size in range(4,6):
+    size *= 100
+    saved_path = "../data/trained_data/rakuten_m1_v" + str(size) + "_min5_w4.vec.pt"
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    model = word2vec.Word2Vec(sentences, epochs=5,vector_size=size,sg=1,min_count=5, window=4, workers=8,hs=0)
+    model.wv.save_word2vec_format(saved_path, binary=True)
